36409dcdfddb509a4c84cca4756dfc13ae498c81
Mr.EIliya <mreiliya333@gmail.com>
2025-12-09 17:08:14 +0330

    fixing the query set
    fixed queryset for task view set

--- core/todo/api/v1/views.py
+++ core/todo/api/v1/views.py
@@ -18,7 +18,7 @@ class TaskModelViewSet(viewsets.ModelViewSet):
     filterset_fields = ['user','is_completed']
     search_fields = ['title']
     ordering_fields = ['published_date']
-    
+        
     def get_queryset(self):
         return Task.objects.filter(user=self.request.user)
 
