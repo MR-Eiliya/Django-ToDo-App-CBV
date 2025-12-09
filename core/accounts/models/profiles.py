5bd5372f21e9aa1128844310a52134093d5d46fa
Mr.EIliya <mreiliya333@gmail.com>
2025-12-09 17:19:59 +0330

    extending and preparation
    extended and seprated user model and profile model of each other for better access and development.
    prepared API part of account app to start writing API authentication for the project.

--- core/accounts/models/profiles.py
+++ core/accounts/models/profiles.py
@@ -1,17 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from .users import CustomUserModel
 
-class CustomUserModel(AbstractUser):
-    email = models.EmailField(unique=True)
-
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["username"]
-
-    def __str__(self):
-        return self.email
-    
 
 # Profile class for better development
 class Profile(models.Model):
