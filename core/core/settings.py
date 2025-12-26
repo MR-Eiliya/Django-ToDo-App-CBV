acf2c52f10332cf6de8f006f36829f035aab9eb2
Mr.EIliya <174944902+MR-Eiliya@users.noreply.github.com>
2025-12-26 18:35:22 +0330

    basic configuration for celery

--- core/core/settings.py
+++ core/core/settings.py
@@ -174,3 +174,11 @@ EMAIL_HOST = "smtp"
 EMAIL_HOST_USER = ""
 EMAIL_HOST_PASSWORD = ""
 EMAIL_PORT = 25
+
+
+
+# celery config
+CELERY_BROKER_URL = "redis://redis_todo:6379/0"
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_TIMEZONE = "Asia/Tehran"
